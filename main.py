87b25681ecc4374a82ac8e7d374502b39adf130a
DiegoAlbertoPerezJuarez <213475@ids.upchiapas.edu.mx>
2024-01-22 02:21:02 -0600

    finished

--- main.py
+++ main.py
@@ -1,69 +1,86 @@
 import math
+import numpy as np
 import random
 from prettytable import PrettyTable
 import matplotlib.pyplot as plt
 import os
 
-def iniciar_algoritmo_genetico(minimizar, individuos_iniciales, poblacion_max, resolucion, limite_inferior, limite_superior, pc, pmi, pmg, num_generaciones):
+def iniciar_algoritmo_genetico(mini, initial, max_population, resolucion, limit_a, limit_b, pc, pmi, pmg, generations):
 
-    historial_generaciones = []
+    history = []
 
-    intervalos = [limite_inferior, limite_superior]
-    rango = limite_superior - limite_inferior
-    puntos = int((rango / resolucion) + 1)
-    print(puntos)
-    bits = math.ceil(math.log2(puntos))
+    interval = [limit_a, limit_b]
+    rango = limit_b - limit_a
+    points = int((rango / resolucion) + 1)
+    initial_pop = []
+    bits = math.ceil(math.log2(points))
     dX = rango / (2 ** bits - 1)
 
-    def convertir_bit_a_numero(binario):
+    def change_to_num(binario):
         return int(binario, 2)
 
-    def generar_x(binario):
-        return intervalos[0] + convertir_bit_a_numero(binario) * dX
+    def gen_x(binario):
+        return interval[0] + change_to_num(binario) * dX
 
-    def evaluar_funcion(x):
-        return ((pow(x, 3) * math.sin(x) / 100) + (pow(x, 2) * math.cos(x)))
+    def eval_func(x):
+        return ((pow(x, 3) * np.sin(x) / 100) + (pow(x, 2) * np.cos(x)))
 
-    def generar_fx_con_binario(binario):
-        x = generar_x(binario)
-        return evaluar_funcion(x)
+    def gen_binary(binario):
+        x = gen_x(binario)
+        return eval_func(x)
 
-    def generar_poblacion_inicial(valores_iniciales):
-        max_bits = max(puntos.bit_length()
+    def gen_initial_populate(valores_iniciales):
+        max_bits = max(points.bit_length()
                         for valor in valores_iniciales)
 
         binarios = [format(valor, f'0{max_bits}b')
                     for valor in valores_iniciales]
         return binarios
 
-    def ordenar_poblacion(poblacion):
+    def sort_population(poblacion):
         poblacion_ordenada = sorted(
-            poblacion, key=generar_fx_con_binario, reverse=minimizar)
+            poblacion, key=gen_binary, reverse=mini)
         return poblacion_ordenada
 
-    def imprimir_poblacion(poblacion):
+    def make_tables(poblacion):
 
-        table = PrettyTable(['Individuo', 'Bits', 'i', 'x', 'f(x)'])
         iterate = 1
         x_vals = []
         fx_vals = []
         for individuo in poblacion:
-            i = convertir_bit_a_numero(individuo)
-            x = generar_x(individuo)
-            fx = generar_fx_con_binario(individuo)
+            i = change_to_num(individuo)
+            x = gen_x(individuo)
+            fx = gen_binary(individuo)
             x_vals.append(x)
             fx_vals.append(fx)
-            table.add_row([iterate, individuo,
-                            i, round(x, 5), round(fx, 5)])
             iterate += 1
+            
+        poblacion_ordenada = sort_population(poblacion)
 
-        print(table)
+        populate = poblacion_ordenada
+        best = populate[0]
+        mejor_fx = gen_binary(best)
+
+        peor_individuo = populate[-1]
+        peor_fx = gen_binary(peor_individuo)
+        x_valsP = np.linspace(limit_a, limit_b, 400)
+        y_valsP = eval_func(x_valsP)
+        
+        mejor_valor_fx = mejor_fx
+        peor_valor_fx = peor_fx
+        mejor_x = x_vals[fx_vals.index(mejor_valor_fx)]
+        peor_x = x_vals[fx_vals.index(peor_valor_fx)]
+        
         plt.figure()
-        plt.scatter(x_vals, fx_vals, marker='o')
+        plt.scatter(x_vals, fx_vals, marker='o', label='Población')
+        plt.scatter(mejor_x, mejor_fx, color='green', marker='o', label='Mejor')
+        plt.scatter(peor_x, peor_fx, color='red', marker='o', label='Peor')
+        plt.plot(x_valsP, y_valsP, label='funcion', color='magenta', linewidth=2)
         plt.title(f'Generación {generacion}')
         plt.xlabel('x')
         plt.ylabel('f(x)')
         plt.grid(True)
+        plt.legend()
 
         image_folder = 'saved_graphs'
         os.makedirs("saved_graphs", exist_ok=True)
@@ -71,43 +88,36 @@ def iniciar_algoritmo_genetico(minimizar, individuos_iniciales, poblacion_max, r
 
         plt.close()
 
-    def evaluar_poblacion(poblacion):
+    def eval_population(poblacion):
 
-        poblacion_ordenada = ordenar_poblacion(poblacion)
+        poblacion_ordenada = sort_population(poblacion)
 
         poblacion = poblacion_ordenada
 
-        tabla_valores = PrettyTable(
-            ['Intervalos', 'Rango', 'bits', 'resolucion', 'puntos', 'dX'])
-        tabla_valores.add_row(
-            [intervalos, rango, bits, resolucion, puntos, dX])
-        print(tabla_valores)
+        make_tables(poblacion)
 
-        imprimir_poblacion(poblacion)
-
-        mejor_individuo = poblacion[0]
-        mejor_fx = generar_fx_con_binario(mejor_individuo)
+        best = poblacion[0]
+        mejor_fx = gen_binary(best)
 
         peor_individuo = poblacion[-1]
-        peor_fx = generar_fx_con_binario(peor_individuo)
+        peor_fx = gen_binary(peor_individuo)
 
-        promedio_fx = sum(generar_fx_con_binario(ind)
+        promedio_fx = sum(gen_binary(ind)
                             for ind in poblacion) / len(poblacion)
 
-        historial_generaciones.append({'mejor': round(mejor_fx, 5), 'peor': round(
+        history.append({'mejor': round(mejor_fx, 5), 'peor': round(
             peor_fx, 5), 'promedio': round(promedio_fx, 5)})
 
         table_historial = PrettyTable(
             ['Generación', 'Mejor', 'Peor', 'Promedio'])
         index = 0
-        for generacion in historial_generaciones:
+        for generacion in history:
             table_historial.add_row(
                 [index, generacion['mejor'], generacion['peor'], generacion['promedio']])
             index += 1
-        print(table_historial)
         return poblacion
 
-    def seleccionar_por_umbral(poblacion, umbral):
+    def select_rnd(poblacion, umbral):
         seleccionados = []
         index = 1
         for ind in poblacion:
@@ -116,12 +126,12 @@ def iniciar_algoritmo_genetico(minimizar, individuos_iniciales, poblacion_max, r
             index += 1
         return seleccionados
 
-    def generar_parejas(seleccionados):
+    def gen_couples(seleccionados):
         parejas = [(ind, random.choice(seleccionados))
                     for ind in seleccionados]
         return parejas
 
-    def cruzar_parejas(parejas):
+    def pair_couples(parejas):
         hijos = []
         index = 1
         for pareja in parejas:
@@ -135,14 +145,14 @@ def iniciar_algoritmo_genetico(minimizar, individuos_iniciales, poblacion_max, r
                 index += 2
         return hijos
 
-    def seleccionar_para_mutacion(hijos):
+    def select_mut(hijos):
         individuos_seleccionados = []
         for hijo in hijos:
             if random.random() <= pmi:
                 individuos_seleccionados.append(hijo)
         return individuos_seleccionados
 
-    def mutar_bits(elemento, prob_mutacion_bits):
+    def mut_bits(elemento, prob_mutacion_bits):
         bits = list(elemento['bits'])
         for i in range(len(bits)):
             if random.random() <= prob_mutacion_bits:
@@ -151,86 +161,48 @@ def iniciar_algoritmo_genetico(minimizar, individuos_iniciales, poblacion_max, r
         elemento['bits'] = ''.join(bits)
         return elemento
 
-    def mutar_hijos(lista):
+    def mut_children(lista):
         lista_mutada = []
         for ind in lista:
-            ind_mutado = mutar_bits(ind, pmg)
+            ind_mutado = mut_bits(ind, pmg)
             lista_mutada.append(ind_mutado)
         return lista_mutada
 
-    def eliminar_duplicados_y_podar(poblacion):
+    def eliminate(poblacion):
         poblacion = list(set(poblacion))
-        poblacion_ordenada = ordenar_poblacion(poblacion)
-        poblacion_podada = poblacion_ordenada[:poblacion_max]
+        poblacion_ordenada = sort_population(poblacion)
+        poblacion_podada = poblacion_ordenada[:max_population]
         return poblacion_podada
+    
+    for i in range(initial):
+        initial_pop = [random.randint(0,points)]
 
-    poblacion = generar_poblacion_inicial(individuos_iniciales)
+    poblacion = gen_initial_populate(initial_pop)
 
-    for generacion in range(num_generaciones):
-        print(f"\nGeneración {generacion}:")
+    for generacion in range(generations):
 
-        poblacion = evaluar_poblacion(poblacion)
+        poblacion = eval_population(poblacion)
 
-        seleccionados = seleccionar_por_umbral(poblacion, pc)
+        seleccionados = select_rnd(poblacion, pc)
 
-        print("\nIndividuos seleccionados por umbral para cruza:")
-        table_seleccionados = PrettyTable(['Individuo', 'Bits'])
-        for ind in seleccionados:
-            table_seleccionados.add_row(
-                [ind['index'], ind['bits']])
-        print(table_seleccionados)
+        parejas = gen_couples(seleccionados)
 
-        parejas = generar_parejas(seleccionados)
+        hijos = pair_couples(parejas)
 
-        print("\nParejas generadas para cruza:")
-        table_parejas = PrettyTable(
-            ['Individuo 1', 'Bits 1', 'Individuo 2', 'Bits 2'])
-        for pareja in parejas:
-            table_parejas.add_row(
-                [pareja[0]['index'], pareja[0]['bits'], pareja[1]['index'], pareja[1]['bits']])
-        print(table_parejas)
+        hijos_seleccionado_para_mutar = select_mut(hijos)
 
-        hijos = cruzar_parejas(parejas)
-
-        print("\nHijos generados después del cruce:")
-        table_hijos = PrettyTable(['Individuo', 'Bits'])
-        for i, hijo in enumerate(hijos, 1):
-            table_hijos.add_row([i, hijo['bits']])
-        print(table_hijos)
-
-        hijos_seleccionado_para_mutar = seleccionar_para_mutacion(hijos)
-
-        print("\nHijos después de selección de quienes mutan:")
-        table_hijos_seleccionados_para_mutar = PrettyTable(
-            ['Individuo', 'Bits'])
-        for i, hijo_seleccionado_para_mutar in enumerate(hijos_seleccionado_para_mutar, 1):
-            table_hijos_seleccionados_para_mutar.add_row(
-                [hijos.index(hijo_seleccionado_para_mutar) + 1, hijo_seleccionado_para_mutar['bits']])
-        print(table_hijos_seleccionados_para_mutar)
-
-        hijos_mutados_bits = mutar_hijos(
+        hijos_mutados_bits = mut_children(
             hijos_seleccionado_para_mutar)
-
-        print("\nHijos después de la mutación a nivel de bits (intercambio de genes):")
-        table_hijos_mutados_bits = PrettyTable(['Individuo', 'Bits'])
-        for i, hijo_mutado_bits in enumerate(hijos_mutados_bits, 1):
-            table_hijos_mutados_bits.add_row(
-                [hijos.index(hijo_mutado_bits) + 1, hijo_mutado_bits['bits']])
-        print(table_hijos_mutados_bits)
 
         poblacion.extend([hijo['bits'] for hijo in hijos_mutados_bits])
 
-        print("\nPoblación resultante después de agregar los hijos mutados:")
-        imprimir_poblacion(poblacion)
+        make_tables(poblacion)
 
-        poblacion = eliminar_duplicados_y_podar(poblacion)
+        poblacion = eliminate(poblacion)
 
-    print("\nPoblación final después de la generación "+str(num_generaciones)+" :")
-    evaluar_poblacion(poblacion)
+    eval_population(poblacion)
 
-    print(historial_generaciones)
-    print("minimizar: ",minimizar)
-    mejor_individuo = {
-        "Binario": poblacion[0], 'i': convertir_bit_a_numero(poblacion[0]), 'f(x):': generar_fx_con_binario(poblacion[0])}
+    best = {
+        'valor': change_to_num(poblacion[0]), 'f(x)': gen_binary(poblacion[0])}
 
-    return [mejor_individuo, historial_generaciones]
+    return [best, history]
--- menu.py
+++ menu.py
@@ -17,34 +17,24 @@ class Interfaz:
 
         self.grafica_frame = ttk.Frame(root)
         self.grafica_frame.grid(row=0, column=1, sticky="nswe", padx=10, pady=10)
+        
+        self.minimizar_var = tk.BooleanVar(value=True)
 
         root.grid_columnconfigure(0, weight=1)
         root.grid_columnconfigure(1, weight=3)
         root.grid_rowconfigure(0, weight=1)
 
-        # Controles
-        self.label_minizar = tk.Label(control_frame, text="Seleccionar qué evaluar:")
-        self.label_minizar.grid(row=0, column=0, padx=5, pady=5, sticky="w")
-
-        self.minimizar_var = tk.BooleanVar(value=True)
-
-        self.radio_minimizar = tk.Radiobutton(control_frame, text="Minimizar", variable=self.minimizar_var, value=True)
-        self.radio_minimizar.grid(row=1, column=0, padx=5, pady=2, sticky="w")
-
-        self.radio_maximizar = tk.Radiobutton(control_frame, text="Maximizar", variable=self.minimizar_var, value=False)
-        self.radio_maximizar.grid(row=2, column=0, padx=5, pady=2, sticky="w")
-
-        self.label_valores_iniciales = tk.Label(control_frame, text="Valores Iniciales (separar por comas):")
+        self.label_valores_iniciales = tk.Label(control_frame, text="Cantidad de la población inicial:")
         self.label_valores_iniciales.grid(row=3, column=0, padx=5, pady=5, sticky="w")
 
-        self.entry_valores_iniciales = tk.Entry(control_frame, width=30)
-        self.entry_valores_iniciales.grid(row=4, column=0, padx=5, pady=2, sticky="w")
+        self.entry_initial_values = tk.Entry(control_frame, width=30)
+        self.entry_initial_values.grid(row=4, column=0, padx=5, pady=2, sticky="w")
 
-        self.label_poblacion_maxima = tk.Label(control_frame, text="Población Máxima:")
+        self.label_poblacion_maxima = tk.Label(control_frame, text="Población máxima:")
         self.label_poblacion_maxima.grid(row=5, column=0, padx=5, pady=5, sticky="w")
 
-        self.entry_poblacion_maxima = tk.Entry(control_frame, width=30)
-        self.entry_poblacion_maxima.grid(row=6, column=0, padx=5, pady=2, sticky="w")
+        self.entry_max_population = tk.Entry(control_frame, width=30)
+        self.entry_max_population.grid(row=6, column=0, padx=5, pady=2, sticky="w")
 
         self.label_resolucion = tk.Label(control_frame, text="Resolución:")
         self.label_resolucion.grid(row=7, column=0, padx=5, pady=5, sticky="w")
@@ -52,106 +42,100 @@ class Interfaz:
         self.entry_resolucion = tk.Entry(control_frame, width=30)
         self.entry_resolucion.grid(row=8, column=0, padx=5, pady=2, sticky="w")
 
-        self.label_limite_inferior = tk.Label(control_frame, text="Límite Inferior:")
+        self.label_limite_inferior = tk.Label(control_frame, text="Intervalo a:")
         self.label_limite_inferior.grid(row=9, column=0, padx=5, pady=5, sticky="w")
 
-        self.entry_limite_inferior = tk.Entry(control_frame, width=30)
-        self.entry_limite_inferior.grid(row=10, column=0, padx=5, pady=2, sticky="w")
+        self.entry_limit_a = tk.Entry(control_frame, width=30)
+        self.entry_limit_a.grid(row=10, column=0, padx=5, pady=2, sticky="w")
 
-        self.label_limite_superior = tk.Label(control_frame, text="Límite Superior:")
+        self.label_limite_superior = tk.Label(control_frame, text="Intervalo b:")
         self.label_limite_superior.grid(row=11, column=0, padx=5, pady=5, sticky="w")
 
-        self.entry_limite_superior = tk.Entry(control_frame, width=30)
-        self.entry_limite_superior.grid(row=12, column=0, padx=5, pady=2, sticky="w")
+        self.entry_limit_b = tk.Entry(control_frame, width=30)
+        self.entry_limit_b.grid(row=12, column=0, padx=5, pady=2, sticky="w")
 
-        self.label_pc = tk.Label(control_frame, text="Probabilidad de Cruce (pc):")
+        self.label_pc = tk.Label(control_frame, text="Probabilidad de cruce:")
         self.label_pc.grid(row=13, column=0, padx=5, pady=5, sticky="w")
 
         self.entry_pc = tk.Entry(control_frame, width=30)
         self.entry_pc.grid(row=14, column=0, padx=5, pady=2, sticky="w")
 
-        self.label_pmi = tk.Label(control_frame, text="Probabilidad de Mutación Individuo (pmi):")
+        self.label_pmi = tk.Label(control_frame, text="Probabilidad de mutación individuo:")
         self.label_pmi.grid(row=15, column=0, padx=5, pady=5, sticky="w")
 
         self.entry_pmi = tk.Entry(control_frame, width=30)
         self.entry_pmi.grid(row=16, column=0, padx=5, pady=2, sticky="w")
 
-        self.label_pmg = tk.Label(control_frame, text="Probabilidad de Mutación Gen (pmg):")
+        self.label_pmg = tk.Label(control_frame, text="Probabilidad de mutación gen:")
         self.label_pmg.grid(row=17, column=0, padx=5, pady=5, sticky="w")
 
         self.entry_pmg = tk.Entry(control_frame, width=30)
         self.entry_pmg.grid(row=18, column=0, padx=5, pady=2, sticky="w")
 
-        self.label_generaciones = tk.Label(control_frame, text="Número de Generaciones:")
+        self.label_generaciones = tk.Label(control_frame, text="Número de generaciones:")
         self.label_generaciones.grid(row=19, column=0, padx=5, pady=5, sticky="w")
 
-        self.entry_generaciones = tk.Entry(control_frame, width=30)
-        self.entry_generaciones.grid(row=20, column=0, padx=5, pady=2, sticky="w")
+        self.entry_generations = tk.Entry(control_frame, width=30)
+        self.entry_generations.grid(row=20, column=0, padx=5, pady=2, sticky="w")
 
-        self.button_iniciar = tk.Button(control_frame, text="Iniciar Algoritmo", command=self.ejecutar_algoritmo)
-        self.button_iniciar.grid(row=21, column=0, padx=5, pady=5, sticky="ew")
+        self.radio_minimizar = tk.Radiobutton(control_frame, text="Minimizar", variable=self.minimizar_var, value=False)
+        self.radio_minimizar.grid(row=21, column=0, padx=5, pady=2, sticky="w")
 
-        self.mejor_individuo = tk.Label(control_frame, text="")
-        self.mejor_individuo.grid(row=0, column=1, padx=5, pady=5, sticky="w")
+        self.radio_maximizar = tk.Radiobutton(control_frame, text="Maximizar", variable=self.minimizar_var, value=True)
+        self.radio_maximizar.grid(row=22, column=0, padx=5, pady=2, sticky="w")
+
+        self.button_iniciar = tk.Button(control_frame, text="Iniciar", command=self.ejecutar_algoritmo)
+        self.button_iniciar.grid(row=23, column=0, padx=5, pady=5, sticky="ew")
+
+        self.best_pop = tk.Label(control_frame, text="")
+        self.best_pop.grid(row=24, column=0, padx=5, pady=5, sticky="w")
         
     def ejecutar_algoritmo(self):
-        texto_ingresado = str(self.minimizar_var.get())
-        valores_iniciales_str = self.entry_valores_iniciales.get().split(',')
-
-        try:
-            valores_iniciales = [int(valor) for valor in valores_iniciales_str]
-        except ValueError:
-            self.label_mensaje.config(
-                text="Error: Ingresa valores iniciales válidos (números enteros).")
-            return
-
-        poblacion_maxima = int(self.entry_poblacion_maxima.get())
-        limite_inferior = float(self.entry_limite_inferior.get())
-        limite_superior = float(self.entry_limite_superior.get())
+        initial_values = int(self.entry_initial_values.get())
+        max_population = int(self.entry_max_population.get())
+        limit_a = float(self.entry_limit_a.get())
+        limit_b = float(self.entry_limit_b.get())
         pc = float(self.entry_pc.get())
         pmi = float(self.entry_pmi.get())
         pmg = float(self.entry_pmg.get())
-        num_generaciones = int(self.entry_generaciones.get())
+        num_generaciones = int(self.entry_generations.get())
         resolucion = float(self.entry_resolucion.get())
 
-        mejor_individuo, historial_generaciones = main.iniciar_algoritmo_genetico(
-            texto_ingresado.lower == 'false',
-            valores_iniciales,
-            poblacion_maxima,
+        best_pop, history = main.iniciar_algoritmo_genetico(
+            self.minimizar_var.get(),
+            initial_values,
+            max_population,
             resolucion,
-            limite_inferior,
-            limite_superior,
+            limit_a,
+            limit_b,
             pc,
             pmi,
             pmg,
             num_generaciones
         )
 
-        self.mejor_individuo.config(
-            text="Mejor individuo"+str(mejor_individuo))
+        self.make_graphics(history, best_pop)
 
-        self.inicializar_grafica(historial_generaciones)
-
-    def inicializar_grafica(self, historial_generaciones):
+    def make_graphics(self, history, best_pop):
         self.figura, self.ax = plt.subplots()
 
-        generaciones = list(range(1, len(historial_generaciones) + 1))
-        mejor = [item['mejor'] for item in historial_generaciones]
-        peor = [item['peor'] for item in historial_generaciones]
-        promedio = [item['promedio'] for item in historial_generaciones]
+        generations_final = list(range(1, len(history) + 1))
+        best = [item['mejor'] for item in history]
+        worst = [item['peor'] for item in history]
+        average = [item['promedio'] for item in history]
 
-        linea_mejor, = self.ax.plot(generaciones, mejor, label='Mejor')
-        linea_peor, = self.ax.plot(generaciones, peor, label='Peor')
+        linea_mejor, = self.ax.plot(generations_final, best, label='Mejor')
+        linea_peor, = self.ax.plot(generations_final, worst, label='Peor')
         linea_promedio, = self.ax.plot(
-            generaciones, promedio, label='Promedio')
-
-        self.ax.legend()
+            generations_final, average, label='Promedio')
         
+        self.ax.legend()
+        self.ax.set_title(f'Mejor: {best_pop}')
         
         image_folder = 'saved_graphs'
 
         image_files = sorted([os.path.join(image_folder, img) for img in os.listdir(image_folder) if img.endswith(".png")])
-        clip = ImageSequenceClip(image_files, fps=5)
+        clip = ImageSequenceClip(image_files, fps=1)
 
         video_file = "output_video.mp4"
         clip.write_videofile(video_file)
